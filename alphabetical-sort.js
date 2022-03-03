d519e0fc837271210231a6e0d28804f6eddd92a7
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-03 13:14:13 +1300

    Create alphabetical-sort.js

---
+++ alphabetical-sort.js
@@ -0,0 +1,8 @@
+// Sort a given array with alphabetics in alphabetical order
+
+function alphabeticalOrder(arr) {
+  const arrSort = arr.sort((a, b) => a === b ? 0 : a < b ? -1 : 1)
+  return arrSort
+}
+
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
