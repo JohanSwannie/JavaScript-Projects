7e7a26a9b2a94da20a2ab110efb7394356724a77
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-05-20 13:49:45 +1200

    Update api-images.js

--- api-images.js
+++ api-images.js
@@ -12,11 +12,11 @@ fetch('https://cdn.shopify.com/s/files/1/0300/3498/5096/products/20200530_113756
 
 // Example 2
 
-catchElephant()
+fetchElephant()
   .then(response => console.log(`The Fetch for the Elephant image wasSuccessfull`))
   .catch(error => console.error(`There is an error with fetching the elephant image - ${error}`));
 
-async function catchElephant() {
+async function fetchElephant() {
   const response = await fetch('https://static.scientificamerican.com/sciam/cache/file/065ACE5F-1E4B-4D3E-8E1E7CE0397D7681.jpg');
   const elephant = await response.blob();
   document.getElementById('elephant').src = URL.createObjectURL(elephant);
