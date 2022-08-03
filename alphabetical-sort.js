73b4d4ca6018ab59cd39579ef41cc1c831ad5c28
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-08-03 18:07:56 +1200

    Update alphabetical-sort.js

--- alphabetical-sort.js
+++ alphabetical-sort.js
@@ -2,7 +2,7 @@
 
 function alphabeticalOrder(arr) {
   const arrSort = arr.sort((a, b) => a === b ? 0 : a < b ? -1 : 1)
-  return arrSort
+  return arrSort  
 }
 
 console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
