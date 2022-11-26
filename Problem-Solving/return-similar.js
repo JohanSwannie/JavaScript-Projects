6fd9d9a123ec2672e563500b655f1eb0b37f9976
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-11-27 07:59:26 +1100

    Update return-similar.js

--- Problem-Solving/return-similar.js
+++ Problem-Solving/return-similar.js
@@ -1,14 +1,14 @@
 // parameter1 = array with objects + parameter2 just objects - return all occurences
 // of paramters 1 that  have similar key value pairs as parameter2
 
-function whatIsInAName(collection, source) {
+function findLastName(firstObjects, source) {
   let arr = [];
   let checkKeys = Object.keys(source);
-  arr = collection.filter(item => checkKeys.every(key => item.hasOwnProperty(key)  && item[key] === source[key]));
+  arr = firstObjects.filter(item => checkKeys.every(key => item.hasOwnProperty(key)  && item[key] === source[key]));
   return arr;
 }
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Juliet" },
+console.log(findLastName([{ first: "Romeo", last: "Juliet" },
                               { first: "Mercutio", last: null },
                               { first: "Tybalt", last: "Capulet", final: 'Oops' }],
                               { last: "Capulet" }));
