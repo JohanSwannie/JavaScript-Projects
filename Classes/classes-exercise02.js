3e5d8ddcab6ab7913328ee2f58cea90adfd6c09e
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-07 09:12:47 +1100

    Create classes-exercise02.js

---
+++ Classes/classes-exercise02.js
@@ -0,0 +1,24 @@
+  class Box {
+    constructor(_width) {
+      this.width = _width;
+      this.height = _width;
+    }
+
+    static test1(width, height) {
+      return `Is the box a square - ${width === height}`;
+    }
+
+    static test2(width, height) {
+      return `Is the square area greater than 200 - ${width * height > 200}`;
+    }
+  }
+
+  let box1 = new Box(12);
+  for (var value in box1) {
+    if (box1.hasOwnProperty(value)) {
+      document.write(`${value} : ${box1[value]}  &nbsp  &nbsp </br>`);
+    }
+  }
+
+  document.write(`${Box.test1(14, 14)} </br>`);
+  document.write(`${Box.test2(13, 13)}`);
