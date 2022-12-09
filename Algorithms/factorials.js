0e1536175f49ae2c55271a754a4aafbae259526d
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-09 13:55:44 +1100

    Update factorials.js

--- Algorithms/factorials.js
+++ Algorithms/factorials.js
@@ -15,7 +15,7 @@ function getFactorial(num) {
 const factorialArray1 = [11, 4, 19, 9, 5];
 
 for (var j = 0; j < factorialArray1.length; j++) {
-  document.write(`<span style="color: #FFF">The Factorial of ${factorialArray1[j]} = ${getFactorial(factorialArray1[j])} </span> </br> </br>`);
+  document.write(`<p style="color: #FFF; text-align: center">The Factorial of ${factorialArray1[j]} = ${getFactorial(factorialArray1[j])} </p> </br>`);
 }
 
 // Factorials Algorithm - Recursive Solution
@@ -30,6 +30,12 @@ function recursiveFactorial(num) {
 
 const factorialArray2 = [7, 3];
 
+const theBody = document.getElementById('theBody');
+
+const h2Another = document.createElement('h2');
+h2Another.textContent = 'Factorials Algorithm - Recursion';
+theBody.appendChild(h2Another);
+
 for (var k = 0; k < factorialArray2.length; k++) {
-  document.write(`<span style="color: #FFF">The Factorial of ${factorialArray2[k]} = ${recursiveFactorial(factorialArray2[k])} </span> </br> </br>`);
+  document.write(`<p style="color: #FFF; text-align: center">The Factorials of ${factorialArray2[k]} = ${recursiveFactorial(factorialArray2[k])} </p> </br> </br>`);
 }
