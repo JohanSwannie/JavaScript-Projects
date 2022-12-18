8e634383e82a83a52a7dfe0d5f40b7a5b2e4b73e
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-18 13:31:04 +1100

    Update timeout-functions1.js

--- Functions/timeout-functions1.js
+++ Functions/timeout-functions1.js
@@ -1,17 +1,11 @@
 function getTheTime() {
-  const clocker1 = document.getElementById('clock1');
-  const clocker2 = document.getElementById('clock2');
-  const clocker3 = document.getElementById('clock3');
-  const clocker4 = document.getElementById('clock4');
-  const clocker5 = document.getElementById('clock5');
+  const clockers = document.querySelectorAll('.clocko');
   return setInterval(() => {
     let todaysDate = new Date();
     let timeDisplay = todaysDate.toLocaleTimeString();
-    clocker1.innerHTML = timeDisplay;
-    clocker2.innerHTML = timeDisplay;
-    clocker3.innerHTML = timeDisplay;
-    clocker4.innerHTML = timeDisplay;
-    clocker5.innerHTML = timeDisplay;
+    clockers.forEach(item => {
+      item.innerHTML = timeDisplay;
+    });
   }, 1000);
 }
 
