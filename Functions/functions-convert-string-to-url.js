dd3599b3be27f937de7f56f973f6f53b47543867
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-20 10:32:44 +1100

    Create functions-convert-string-to-url.js

---
+++ Functions/functions-convert-string-to-url.js
@@ -0,0 +1,11 @@
+function converToURL(stringValue) {
+  const specialCharacters = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+  const stringValueWithoutSpecialCharacters = stringValue.replace(specialCharacters, "");
+  return stringValueWithoutSpecialCharacters.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+let string1 = 'The#* main task$ for us to ()( complete is to *)resolve user issues!!!';
+let string2 = 'The#* rest of the tasks$ for us to ()( complete is to *)resolve other issues!!!';
+
+document.getElementById('string1').innerHTML = converToURL(string1);
+document.getElementById('string2').innerHTML = converToURL(string2);
