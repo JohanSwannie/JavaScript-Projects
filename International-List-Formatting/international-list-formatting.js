65229a8e18a2e8c9b4077fe1cd62ecba23944237
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-06 09:01:01 +1100

    Create international-list-formatting.js

---
+++ International-List-Formatting/international-list-formatting.js
@@ -0,0 +1,26 @@
+
+    let array1 = ['Jason', 'Freddy', 'John', 'Mike'];
+
+    let formatter1 = new Intl.ListFormat("en-nz")
+
+    let formatter2 = new Intl.ListFormat("en-nz", {
+      type: "disjunction",
+    });
+
+    let formatter3 = new Intl.ListFormat("en-us", {
+      style: "short",
+    });
+
+    let formatter4 = new Intl.ListFormat("en", {
+      style: "narrow",
+    });
+
+    let formatter5 = new Intl.ListFormat("en", {
+      style: "narrow", type: "unit",
+    });
+
+    document.getElementById('ftxt1').innerHTML = formatter1.format(array1);
+    document.getElementById('ftxt2').innerHTML = formatter2.format(array1);
+    document.getElementById('ftxt3').innerHTML = formatter3.format(array1);
+    document.getElementById('ftxt4').innerHTML = formatter4.format(array1);
+    document.getElementById('ftxt5').innerHTML = formatter5.format(array1);
