6a0f9cf54618b10ea53cc8e9146b852591d3e4b6
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-06 10:55:21 +1100

    Update binary-search.js

--- Search-Algorithms/binary-search.js
+++ Search-Algorithms/binary-search.js
@@ -1,12 +1,12 @@
-function binarySearch(array, value) {
+function binarySearch(binny, value) {
   let indexStart = 0;
-  let indexEnd = array.length - 1;
+  let indexEnd = binny.length - 1;
   while (indexStart < indexEnd) {
     let indexCenter = Math.floor((indexStart + indexEnd) / 2);
-    if (value === array[indexCenter]) {
+    if (value === binny[indexCenter]) {
       return `The value ${value} is found at index ${indexCenter}`;
     }
-    if (value > array[indexCenter]) {
+    if (value > binny[indexCenter]) {
       indexStart = indexCenter + 1;
     } else {
       indexEnd = indexCenter -1;
@@ -15,8 +15,8 @@ function binarySearch(array, value) {
   return `The value ${value} is not found in the array`;
 }
 
-let array1 = [29.55, 1.3, 35.65, 7.65, 17.33, 84, 10.43, 19.88, 41.75, 12.44, 51, 1.89];
+let initialArray = [29.55, 1.3, 35.65, 7.65, 17.33, 84, 10.43, 19.88, 41.75, 12.44, 51, 1.89];
 
-let array2 = array1.sort((a, b) => a - b);
+let binarySearchArray = initialArray.sort((a, b) => a - b, 0);
 
-document.write(`<p style="color: #FFF; font-size: 28px; text-align: center">${binarySearch(array2, 10.43)}</p>`);
+document.write(`<p style="color: #FFF; font-size: 28px; text-align: center">${binarySearch(binarySearchArray, 10.43)}</p>`);
